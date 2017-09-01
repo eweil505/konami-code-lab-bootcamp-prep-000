2ecc7e90cda2191eccc4728acae54ff289757bd3
Erika Weil <erikaweil@outlook.com>
2017-09-01 23:24:02 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,9 +1,11 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-let index = 0;
 
-function init(e) {
+
+function init() {
+  document.body.addEventListener('keydown', function(e) {
     const key = (e.detail || e.which);
-    console.log(key);
+    let index = 0;
+    // console.log(key);
     if (key === code[index]) {
       index++;
       if (index === code.length) {
@@ -13,6 +15,7 @@ function init(e) {
     } else {
       index = 0;
     }
+})
 }
 
 document.body.addEventListener('keydown', init)
