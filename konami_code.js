10ad613b0d199ed8c0d90f391ea2e53f75e8ea77
Erika Weil <erikaweil@outlook.com>
2017-09-01 23:26:08 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,10 +1,11 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-let index = 1;
+let index = 0;
 
 function init() {
   document.body.addEventListener('keydown', function(e) {
     const key = (e.detail || e.which);
-    // console.log(key);
+    console.log(`the index is ${index}`)
+    console.log(`the key you pressed is ${key}`);
     if (key === code[index]) {
       index++;
       if (index === code.length) {
